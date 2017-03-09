ba1a827f36141c03e51f8b614b7b7901a4ee3a47
Steven Li <Steven Li>
2017-03-09 17:36:06 -0500

    Corrected return logic

--- ResearchGateSpider/spiders/RGSpider1.py
+++ ResearchGateSpider/spiders/RGSpider1.py
@@ -28,13 +28,15 @@ class RGSpider1(CrawlSpider):
 
     def parse_candidate_overview(self, response):
         if response.status == 429:
-            lostitem_str = 'lost overview: ' + response.url
-            self.lostitem_file.write(lostitem_str)
-            self.lostitem_file.close()
+            # lostitem_str = 'lost overview: ' + response.url
+            # self.lostitem_file.write(lostitem_str)
+            # self.lostitem_file.close()
             raise CloseSpider(reason='被封了，准备切换ip')
         print '-----------start to process: ' + response.url
         headers = response.request.headers
         headers["referer"] = response.url
+
+        item = ResearchGateItem()
 
         featured_researches = response.xpath('//div[contains(@class, "profile-highlights-publications")]').extract()
         address = DataFilter.simple_format(response.xpath('//div[contains(@class, "institution-location")]/text()').extract())
@@ -56,37 +58,33 @@ class RGSpider1(CrawlSpider):
             city = address
             province = ''
             country = ''
+        item['person_key'] = request_fingerprint(response.request)
+        item['fullname'] = DataFilter.simple_format(response.xpath('//a[@class = "ga-profile-header-name"]/text()').extract())
+        item['target_sciences'] = DataFilter.simple_format(response.xpath('//*[@id="target-sciences"]/text()').extract())
+        item['title'] = DataFilter.simple_format(response.xpath('//*[contains(@class,"profile-degree")]/div[@class="title"]/text()').extract())
+        item['score'] = DataFilter.simple_format(response.xpath('//span[starts-with(@class, "score-link")]').extract())
+
+        top_coauthors = response.xpath('//div[starts-with(@class, "authors-block")]//ul/li//h5[@class="ga-top-coauthor-name"]/a')
+        item['co_authors'] = parse_text_by_multi_content(top_coauthors, "|")
+        
+        skills_expertise = response.xpath('//div[starts-with(@class, "profile-skills")]/ul/li//a[starts-with(@class, "keyword-list-token-text")]')
+        item['skills'] = parse_text_by_multi_content(skills_expertise, "|")
+
+        topics = response.xpath('//ul[@class="keyword-list clearfix"]/li//a[starts-with(@class, "keyword-list-token-text")]')
+        item['topics'] = parse_text_by_multi_content(topics, "|")
+
+        item['institution'] = DataFilter.simple_format(response.xpath('//div[starts-with(@class, "institution-name")]').extract())
+        item['department'] = DataFilter.simple_format(response.xpath('//div[@class = "institution-dept"]').extract())
+        
+        item['city'] = city
+        item['province'] = province
+        item['country'] = country
         if featured_researches and country != 'China': 
-            item = ResearchGateItem()
-
-            item['person_key'] = request_fingerprint(response.request)
-            item['fullname'] = DataFilter.simple_format(response.xpath('//a[@class = "ga-profile-header-name"]/text()').extract())
-            item['target_sciences'] = DataFilter.simple_format(response.xpath('//*[@id="target-sciences"]/text()').extract())
-            item['title'] = DataFilter.simple_format(response.xpath('//*[contains(@class,"profile-degree")]/div[@class="title"]/text()').extract())
-            item['score'] = DataFilter.simple_format(response.xpath('//span[starts-with(@class, "score-link")]').extract())
-
-            top_coauthors = response.xpath('//div[starts-with(@class, "authors-block")]//ul/li//h5[@class="ga-top-coauthor-name"]/a')
-            item['co_authors'] = parse_text_by_multi_content(top_coauthors, "|")
-            
-            skills_expertise = response.xpath('//div[starts-with(@class, "profile-skills")]/ul/li//a[starts-with(@class, "keyword-list-token-text")]')
-            item['skills'] = parse_text_by_multi_content(skills_expertise, "|")
-
-            topics = response.xpath('//ul[@class="keyword-list clearfix"]/li//a[starts-with(@class, "keyword-list-token-text")]')
-            item['topics'] = parse_text_by_multi_content(topics, "|")
-
-            item['institution'] = DataFilter.simple_format(response.xpath('//div[starts-with(@class, "institution-name")]').extract())
-            item['department'] = DataFilter.simple_format(response.xpath('//div[@class = "institution-dept"]').extract())
-            
-            item['city'] = city
-            item['province'] = province
-            item['country'] = country
-
             url = response.url + "/publications"
             print url
-            return Request(url, headers=headers, callback=self.parse_contribution, dont_filter=True, meta={"item":item})
+            yield Request(url, headers=headers, callback=self.parse_contribution, dont_filter=True, meta={"item":item})
         else:
             print "--------Nothing to return, it is invalid--------"
-        # yield Request(url, callback=self.parse_contribution, dont_filter=True, meta={"item":item})
 
     def parse_contribution(self, response):
         if response.status == 429:
@@ -94,6 +92,8 @@ class RGSpider1(CrawlSpider):
             self.lostitem_file.write(lostitem_str)
             self.lostitem_file.close()
             raise CloseSpider(reason=u'被封了，准备切换ip')
+
+
         headers = response.request.headers
         headers["referer"] = response.url
         # Parse articles, each article has a seperate page
@@ -110,14 +110,6 @@ class RGSpider1(CrawlSpider):
         for article_url in article_urls:
             article_url = self.domain + "/" + article_url
             yield Request(article_url, headers=headers, callback=self.parse_article, dont_filter=True, meta={'item':item, 'count':article_count})
-        # for url in = article_urls[:3]:
-        #     url = self.domain + "/" + url
-        #     yield Request(url, headers=headers, callback=self.parse_article, dont_filter=True, meta={'item':item, 'last':'no'})
-        
-        
-        # urls = response.xpath('//li[contains(@class, "li-publication")]/descendant::a[contains(@class, "js-publication-title-link")]/@href').extract()
-        # url0 = self.domain + "/" + urls[0]
-        # yield Request(url0, headers=headers, callback=self.parse_article, dont_filter=True)
 
     def parse_article(self, response):
         if response.status == 429:
@@ -126,7 +118,6 @@ class RGSpider1(CrawlSpider):
             self.lostitem_file.close()
             raise CloseSpider(reason='被封了，准备切换ip')
         print response.url
-        # item = ResearchGateItem()
         item = response.meta['item']
         pub_count = response.meta['count']
         
@@ -139,6 +130,8 @@ class RGSpider1(CrawlSpider):
         article_date = DataFilter.simple_format(response.xpath('//span[@class="publication-meta-date"]').extract())
         article_item['article_journal'] = article_journal + ", " + article_date
         item['publications'].append(article_item)
+
+        print "**********Handled %i of %i articles" % (len(item['publications']), pub_count)
         if len(item['publications']) == pub_count:
             return item
     
